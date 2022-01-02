56ae8b28f7756c6325ccf4c038493348f40a7fe7
Rashedul Islame <rasheduzzaman521@gmail.comm>
2022-01-02 22:36:09 +0600

    novel-data

--- src/Data/Writer/Writer.js
+++ src/Data/Writer/Writer.js
@@ -47,6 +47,42 @@ const writer = [
       "https://www.kalantorprokashoni.com/wp-content/uploads/2019/09/kazi-abul-kalam-siddique-308x308.jpg",
     writerName: "কাজী আবুল কালাম সিদ্দীক",
   },
+  {
+    id: 9,
+    writerImg:
+      "https://s3-ap-southeast-1.amazonaws.com/rokomari110/people/feecb90af_78860.jpg",
+    writerName: "শায়খ আহমাদুল্লাহ",
+  },
+  {
+    id: 10,
+    writerImg:
+      "https://s3-ap-southeast-1.amazonaws.com/rokomari110/people/2222ed505a94_1.jpg",
+    writerName: "হুমায়ূন আহমেদ",
+  },
+  {
+    id: 11,
+    writerImg:
+      "https://ds.rokomari.store/rokomari110/people/d4e09a6978f4_24701.jpg",
+    writerName: "ড. খোন্দকার আব্দুল্লাহ জাহাঙ্গীর",
+  },
+  {
+    id: 12,
+    writerImg:
+      "https://s3-ap-southeast-1.amazonaws.com/rokomari110/people/f05844bf5ab4_40266.jpg",
+    writerName: "ঝংকার মাহবুব",
+  },
+  {
+    id: 13,
+    writerImg:
+      "https://s3-ap-southeast-1.amazonaws.com/rokomari110/people/db5c4d8f1_6019.jpg",
+    writerName: "চমক হাসান",
+  },
+  {
+    id: 14,
+    writerImg:
+      "https://ds.rokomari.store/rokomari110/people/98ec0cea7_4207.jpg",
+    writerName: "তামিম শাহরিয়ার সুবিন",
+  },
 ];
 
 export default writer;
